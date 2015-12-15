87a14f099e6d60bbf671b5f2607fb8f6de7cea79
SergeevSergey <sergeevsergeyw@gmail.com>
2015-12-15 18:13:14 +0300

    обработчик ошибок сделан

--- app.rb
+++ app.rb
@@ -1,27 +1,45 @@
 require 'rubygems'
 require 'sinatra'
+
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
 get '/about' do
-	erb :about
+  @error = 'something wrong!'
+  erb :about
 end
 
 get '/visit' do
-	erb :visit
+  erb :visit
 end
 
 post '/visit' do
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
 
-	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+  hh= {:username => 'Введите имя',
+	   :phone => 'Введите номер телефона',
+	   :datetime => 'Неправильная дата и время'}
+
+  @error = hh.select {|key| params[key] == ""}.values.join(", ")
+#hh.each do |k,v|
+	#if params[k] == ''
+	if @error != ''
+		return erb :visit
+	end
+#end
+
+
+
+
+  erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
